33b3e5b8155896093527ef92f0f91686796731e8
ShyNebulas <cameron352bell@gmail.com>
2023-11-16 22:26:38 +0000

    feat : 7 -> 8 (Late commit)

--- src/com/shynebulas/jlox/Lox.java
+++ src/com/shynebulas/jlox/Lox.java
@@ -8,7 +8,9 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 public class Lox {
+    private static final Interpreter interpreter = new Interpreter();
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
     public static void main(String[] args) throws IOException {
         if(args.length > 1) {
             System.out.println("Usage: jlox [script]");
@@ -25,6 +27,7 @@ public class Lox {
         run(new String(bytes, Charset.defaultCharset()));
         // https://manpages.ubuntu.com/manpages/lunar/man3/sysexits.h.3head.html
         if (hadError) System.exit(65);
+        if (hadRuntimeError) System.exit(70);
     }
 
     private static void runPrompt() throws IOException {
@@ -48,7 +51,7 @@ public class Lox {
         // Stop if there was a syntax error
         if(hadError) return;
 
-        System.out.println(new AstPrinter().print(expression));
+        interpreter.interpret(expression);
     }
     static void error(int line, String message) {
         report(line, "", message);
@@ -64,6 +67,13 @@ public class Lox {
             report(token.line, " at'" + token.lexeme + "'", message);
         }
     }
+
+    static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() + "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
+    }
+
+
 }
 
 
